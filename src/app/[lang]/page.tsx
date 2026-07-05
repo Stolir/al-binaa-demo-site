7972da28de06a4d3c95770e4d627af6ffad981d8
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-07-05 16:49:21 +0300

    Add hero section to home page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,9 +1,21 @@
+import HeroSection from "@/components/HeroSection/HeroSection";
 import styles from "./page.module.css";
+import { Lang } from "@/lib/types";
+import { getDictionary } from "@/lib/utils";
 
-export default function Home() {
+interface HomeProps {
+  params: Promise<{ lang: Lang }>;
+}
+
+export default async function Home({ params }: HomeProps) {
+  const { lang } = await params;
+
+  const content = getDictionary(lang);
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <HeroSection content={content.hero} />
+      </main>
     </div>
   );
 }
