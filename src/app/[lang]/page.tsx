a6a585992a5531d0322098a7236ca801fb2a7ec6
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2026-07-06 13:23:47 +0300

    Add StatBar to home page

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/HeroSection/HeroSection";
 import styles from "./page.module.css";
 import { Lang } from "@/lib/types";
 import { getDictionary } from "@/lib/utils";
+import StatBar from "@/components/StatBar/StatBar";
 
 interface HomeProps {
   params: Promise<{ lang: Lang }>;
@@ -15,6 +16,7 @@ export default async function Home({ params }: HomeProps) {
     <div className={styles.page}>
       <main className={styles.main}>
         <HeroSection content={content.hero} />
+        <StatBar content={content.statBar} />
       </main>
     </div>
   );
